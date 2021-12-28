5acd0d8abfc7feed4cbbfa8481e958279860a0e4
Muslimbek Abduvaliev <84398564+Programmer-707@users.noreply.github.com>
2021-12-28 20:58:49 +0300

    Add files via upload

---
+++ bfs_dfs.cpp
@@ -0,0 +1,111 @@
+
+#include <iostream>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *next;
+
+}*front=NULL,*rear=NULL;
+
+void enqueue(int x)
+{
+    struct Node *t;
+    t = new Node;
+    if(t==NULL)
+        cout << "Queue is FUll\n";
+    else
+    {
+        t->data=x;
+        t->next=NULL;
+        if(front==NULL)
+            front=rear=t;
+        else
+        {
+            rear->next=t;
+            rear=t;
+        }
+    }
+
+}
+int dequeue()
+{
+    int x=-1;
+    struct Node* t;
+
+    if(front==NULL)
+        cout << "Queue is Empty\n";
+    else
+    {
+        x=front->data;
+        t=front;
+        front=front->next;
+        free(t);
+    }
+    return x;
+}
+int isEmpty()
+{
+    return front==NULL;
+}
+
+//BFS
+void BFS(int G[][7],int start,int n)
+{
+    int i=start,j;
+
+    int visited[7]= {0};
+
+    cout<< i << ", ";
+    visited[i]=1;
+    enqueue(i);
+
+    while(!isEmpty())
+    {
+        i=dequeue();
+        for(j=1; j<n; j++)
+        {
+            if(G[i][j]==1 && visited[j]==0)
+            {
+                cout << j <<", ";
+                visited[j]=1;
+                enqueue(j);
+            }
+        }
+    }
+
+
+}
+void DFS(int G[][7],int start,int n)
+{
+    static int visited[7]= {0};
+    int j;
+
+    if(visited[start]==0)
+    {
+        cout <<start << ", ";
+        visited[start]=1;
+
+        for(j=1; j<n; j++)
+        {
+            if(G[start][j]==1 && visited[j]==0)
+                DFS(G,j,n);
+        }
+    }
+}
+int main()
+{
+    int G[7][7]= {{0,0,0,0,0,0,0},
+        {0,0,1,1,0,0,0},
+        {0,1,0,0,1,0,0},
+        {0,1,0,0,1,0,0},
+        {0,0,1,1,0,1,1},
+        {0,0,0,0,1,0,0},
+        {0,0,0,0,1,0,0}
+    };
+    DFS(G,2,7);
+
+    return 0;
+}
